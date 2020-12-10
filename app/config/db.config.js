25a0614dc619d15aa0d0579a8ca22ed8d9685e5e
Prasetya Dhany Putra <putraprasetyadhany@gmail.com>
2020-12-10 15:55:57 +0700

    hasil

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    Host: "localhost",
+    USER: "prasetya",
+    PASSWORD: "1234",
+    DB: "testdb",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle:10000
+    }
+};
